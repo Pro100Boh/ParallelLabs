30552883e0390cf86e755d8cc5d90110804376e6
Pro100Boh <veres.bs@gmail.com>
2020-02-15 18:38:45 +0200

    BenchmarkDotNet

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,114 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using BenchmarkDotNet.Running;
+using System;
 
 namespace Lab1
 {
     class Program
     {
-        static readonly Random random = new Random();
-
-        const int minRandomValue = -10;
-        const int maxRandomValue = 10;
-        const int vectorLength = 10_000_000;
-
         static void Main(string[] args)
         {
-            var vector = CreateArrayWithRandomValues(vectorLength);
+            var summary = BenchmarkRunner.Run<SequentialVsParallel>();
 
-            var timeSequential = SequentialAlgorithmTest(vector);
-
-            var timeParallel = ParallelAlgorithmTest(vector, 4);
-
-            double diff = timeSequential - timeParallel;
-            Console.WriteLine($"\nDiff(ms) = {diff} ({diff / timeSequential * 100} %)");
-
+            //PrintResults();
 
             Console.ReadKey();
         }
 
-        static long SequentialAlgorithmTest(int[] vector)
+        static void PrintResults()
         {
-            var stopwatch = new Stopwatch();
-            stopwatch.Start();
+            int n = 1_000;
+            var vector = new int[n];
+            var random = new Random();
 
-            int result = CalculateNorm(vector);
-
-            stopwatch.Stop();
-
-            PrintStats("Sequential", stopwatch.ElapsedMilliseconds, result);
-
-            return stopwatch.ElapsedMilliseconds;
-        }
-
-        static long ParallelAlgorithmTest(int[] vector, int numOfThreads)
-        {
-            var stopwatch = new Stopwatch();
-            stopwatch.Start();
-
-            int result = CalculateNormParallel(vector, numOfThreads);
-
-            stopwatch.Stop();
-
-            PrintStats($"Parallel (num of threads = {numOfThreads})", stopwatch.ElapsedMilliseconds, result);
-
-            return stopwatch.ElapsedMilliseconds;
-        }
-
-        static int[] CreateArrayWithRandomValues(int size)
-        {
-            var arr = new int[size];
-
-            for (int i = 0; i < size; i++)
+            for (int i = 0; i < n; i++)
             {
-                arr[i] = random.Next(minRandomValue, maxRandomValue);
+                vector[i] = random.Next(-10, 11);
             }
 
-            return arr;
+            Console.WriteLine($"Sequential = {SequentialVsParallel.CalculateNorm(vector)}");
+            Console.WriteLine($"Parallel (2 threads) = {SequentialVsParallel.CalculateNormParallel(vector, 2)}");
+            Console.WriteLine($"Parallel (4 threads) = {SequentialVsParallel.CalculateNormParallel(vector, 4)}");
+            Console.WriteLine($"Parallel (8 threads) = {SequentialVsParallel.CalculateNormParallel(vector, 8)}");
         }
 
-        static int CalculateNorm(int[] vector)
-        {
-            return vector.Sum(Math.Abs);
-        }
 
-        static int CalculateNormParallel(int[] vector, int numOfThreads)
-        {
-            int result = 0;
-            int partSize = vector.Length / numOfThreads;
 
-            Task[] tasks = new Task[numOfThreads];
-
-            for (int i = 0; i < numOfThreads; i++)
-            {
-                int ii = i;
-
-                tasks[ii] = Task.Run(() =>
-                {
-                    int localResult = 0;
-                    for (int j = ii * partSize; j < (ii + 1) * partSize; j++)
-                    {
-                        localResult += Math.Abs(vector[j]);
-                    }
-                    Interlocked.Add(ref result, localResult);
-                });
-            }
-
-            Task.WaitAll(tasks);
-
-            return result;
-        }
-
-        static void PrintStats(string algorithmName, long time, int result)
-        {
-            Console.WriteLine();
-            Console.WriteLine(algorithmName);
-            Console.WriteLine($"Time: {time}");
-            Console.WriteLine($"Result = {result}");
-            Console.WriteLine();
-        }
     }
 }
---
+++ Lab1/SequentialVsParallel.cs
@@ -0,0 +1,86 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    [SimpleJob(launchCount: 1, warmupCount: 1, targetCount: 1)]
+    [HtmlExporter]
+    public class SequentialVsParallel
+    {
+        private int[] _vector;
+
+        [Params(1_000, 10_000, 1_000_000)]
+        public int N { get; set; }
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _vector = new int[N];
+            var random = new Random();
+            
+            for (int i = 0; i < N; i++)
+            {
+                _vector[i] = random.Next(-10, 11);
+            }
+        }
+
+        [Benchmark(Description = "Sequential", Baseline = true)]
+        public void CalculateNormSequential()
+        {
+            CalculateNorm(_vector);
+        }
+        
+        [Benchmark(Description = "Parallel (2 threads)")]
+        public void CalculateNormParallel2Threads()
+        {
+            CalculateNormParallel(_vector, 2);
+        }
+
+        [Benchmark(Description = "Parallel (4 threads)")]
+        public void CalculateNormParallel4Threads()
+        {
+            CalculateNormParallel(_vector, 4);
+        }
+
+        [Benchmark(Description = "Parallel (8 threads)")]
+        public void CalculateNormParallel8Threads()
+        {
+            CalculateNormParallel(_vector, 8);
+        }
+
+        public static int CalculateNorm(int[] vector)
+        {
+            return vector.Sum(Math.Abs);
+        }
+
+        public static int CalculateNormParallel(int[] vector, int threads)
+        {
+            int result = 0;
+            int partSize = vector.Length / threads;
+
+            var tasks = new Task[threads];
+
+            for (int i = 0; i < threads; i++)
+            {
+                int ii = i;
+
+                tasks[ii] = Task.Run(() =>
+                {
+                    int localResult = 0;
+                    for (int j = ii * partSize; j < (ii + 1) * partSize; j++)
+                    {
+                        localResult += Math.Abs(vector[j]);
+                    }
+                    Interlocked.Add(ref result, localResult);
+                });
+            }
+
+            Task.WaitAll(tasks);
+
+            return result;
+        }
+    }
+}
